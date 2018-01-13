5f3cfccd66237f801198651de631a948ebc47b73
Detrav <witalyezep@gmail.com>
2018-01-13 14:48:45 +0300

    * dump version

--- src/main/java/com/detrav/DetravScannerMod.java
+++ src/main/java/com/detrav/DetravScannerMod.java
@@ -25,7 +25,7 @@ import net.minecraftforge.common.config.Configuration;
 public class DetravScannerMod
 {
     public static final String MODID = "detravscannermod";
-    public static final String VERSION = "0.47";
+    public static final String VERSION = "0.50";
 
     public static final CreativeTabs TAB_DETRAV = new DetravCreativeTab();
 
