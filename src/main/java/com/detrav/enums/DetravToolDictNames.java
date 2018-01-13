6ca028859f24af40eda5e2113a249ee5b985f1c1
Detrav <witalyezep@gmail.com>
2018-01-13 14:47:00 +0300

    - fixed oreDictNames

--- src/main/java/com/detrav/enums/DetravToolDictNames.java
+++ src/main/java/com/detrav/enums/DetravToolDictNames.java
@@ -4,11 +4,22 @@ package com.detrav.enums;
  * Created by wital_000 on 19.03.2016.
  */
 public enum  DetravToolDictNames {
-    craftingToolProPick,
-    craftingToolElectricProPick,
-    craftingToolPortableCharger,
-    craftingToolPortableAnvil,
-    craftingToolSmartPlunger,
-    craftingToolSmartTreeTap,
-    craftingToolCictuitConfigurator;
+    craftingToolProPick("craftingToolProPick"),
+    craftingToolElectricProPick("craftingToolElectricProPick"),
+    craftingToolPortableCharger("craftingToolPortableCharger"),
+    craftingToolPortableAnvil("craftingToolPortableAnvil"),
+    craftingToolSmartPlunger("craftingToolSmartPlunger"),
+    craftingToolSmartTreeTap("craftingToolSmartTreeTap");
+
+    private String mValue;
+
+    DetravToolDictNames(String aValue)
+    {
+        mValue = aValue;
+    }
+
+    @Override
+    public String toString() {
+        return this.mValue;
+    }
 }
